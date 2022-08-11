6262b87970433e9f38113b78e9ddaebe988bf640
Anil Gurung <anilgurung@anils-mbp.mynetworksettings.com>
2022-08-11 13:11:30 -0400

    changed the score to 100

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ btnHold.addEventListener("click", function () {
       scores[activePlayer];
 
     //2. check scores is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -100,4 +100,4 @@ btnHold.addEventListener("click", function () {
   }
 });
 
-btnNew.addEventListener("click", function () {});
+btnNew.addEventListener("click", init);
